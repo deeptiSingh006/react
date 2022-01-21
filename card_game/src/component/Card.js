79888c9314fed53808ebc6ab446a490a1e5ac72e
Deepti Singh <88488506+deeptiSingh006@users.noreply.github.com>
2022-01-22 03:37:50 +0530

    BlackJack

--- card_game/src/component/Card.js
+++ card_game/src/component/Card.js
@@ -1,40 +1,98 @@
 import React from 'react';
-import {useState} from 'react';
-function Card(){
+import { useState, useEffect, useRef } from 'react';
+import deck from './Deck';
+function Card() {
+    let card_index;
 
 
-    return(
-       
-<div> 
-       <label >Computer cards</label>   
-       <div class="p-10 grid grid-cols-3 gap-2">
-       <div class="rounded overflow-hidden shadow-lg">
-       <img class="w-full" src="/mountain1.jpg" alt="Mountain"></img></div>
-      < div class="rounded overflow-hidden shadow-lg">
-       <img class="w-full" src="/mountain2.jpg" alt="Mountain"></img></div>
-      < div class="rounded overflow-hidden shadow-lg">
-       <img class="w-full" src="/mountain3.jpg" alt="Mountain"></img></div>
-       </div>
-       <label >Player cards</label>   
-       <div class="p-10  grid grid-cols-3 gap-2">
-       <div class="rounded overflow-hidden shadow-lg">
-       <img class="w-full" src="/1.jpg" alt="Mountain"></img></div>
-      < div class="rounded overflow-hidden shadow-lg">
-       <img class="w-full" src="/2.jpg" alt="Mountain"></img></div>
-      < div class="rounded overflow-hidden shadow-lg">
-       <img class="w-full" src="/3.jpg" alt="Mountain"></img></div>
-       </div>
-       <div class="px-6 pt-4 pb-2">
-        <button class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#DEAL</button>
-        <button class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#SKIP</button>
-       </div> 
-       <div class="px-10 pt-4 pb-2">
-       <h3>score is :</h3>
-       </div>
+    const [deckcards, setdeckcards] = useState(deck);
+    const [cc, setCC] = useState([]);
+    const [pc, setPC] = useState([]);
+    const [dealNum, setDealNum] = useState(0);
+    const [winner,setWinner]=useState();
 
-</div>
-     
-     
+    useEffect(() => {
+            console.log("Player Cards");
+            console.log(pc);
+
+            console.log("Computer Cards");
+            console.log(cc);
+
+            let sumCC = 0
+            for (let i = 0; i < cc.length; i++) {
+                sumCC = sumCC + Number(cc[i].value);
+            }
+
+            let sumPC = 0
+            for (let i = 0; i < pc.length; i++) {
+                sumPC = sumPC + Number(pc[i].value);
+            }
+
+            if (sumPC > 21 || sumCC > 21) {
+                if (Math.abs(21 - sumPC) < Math.abs(21 - sumCC)) {
+                    console.log("Player Won!");
+                    setWinner("Player Won!");
+                }
+                else {
+                    console.log("Computer Won!");
+                    setWinner("Computer Won!");
+                }
+            }
+    }, [cc]);
+
+
+
+    useEffect(() => {
+        if(dealNum >0 ){
+       card_index = Math.floor(Math.random() * deckcards.length)
+        let card=deckcards[card_index]
+        setCC(cc => [...cc, card])
+        }
+    },[pc]);
+
+
+
+    function handleDeal(event) {
+        event.preventDefault();
+        setDealNum(1)
+        card_index = Math.floor(Math.random() * deckcards.length)
+        let card=deckcards[card_index]
+        setdeckcards([...deckcards]);
+        //update dealcount 
+
+        setPC(pc => [...pc, card]);
+    }
+
+    return (
+
+        <div>
+            <label >Computer cards</label>
+            <div className="p-10 grid grid-cols-3 gap-2">
+                {cc.map((a)=>{
+                    let name=a.value+a.suit[0].toUpperCase()+'.png'
+                    return (<div className="rounded overflow-hidden shadow-lg  "><img className="h-25 w-40" src={name} alt={name}></img></div>)})
+                }
+
+            </div>
+            <label >Player cards</label>
+            <div className="p-10 grid grid-cols-3 gap-2">
+                {pc.map((a)=>{
+                    let name=a.value+a.suit[0].toUpperCase()+'.png'
+                    return (<div className="rounded overflow-hidden shadow-lg  "><img className="h-25 w-40" src={name} alt={name}></img></div>)})
+                }
+
+            </div>
+            <div className="px-6 pt-4 pb-2">
+                <button className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2" onClick={handleDeal}>#DEAL</button>
+                <button className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2" >#SKIP</button>
+            </div>
+            <div className="px-10 pt-4 pb-2">
+                <h3 className=" inline-block  border-8 border-double bg-gray-200 rounded-full px-3 py-1 text-2xl font-bold text-gray-700 mr-2 mb-2" >score is : {winner}</h3>
+            </div>
+
+        </div>
+
+
     );
 }
 export default Card;
